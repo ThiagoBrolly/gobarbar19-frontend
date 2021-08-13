3e13d42e2d1b85133e7d80454f5abc37b3c056bc
Thiago Brolly <thiagoex377@gmail.com>
2021-08-13 15:13:38 -0300

    Configurações iniciais

---
+++ src/styles/global.js
@@ -0,0 +1,45 @@
+import { createGlobalStyle } from 'styled-components';
+
+// import 'react-toastify/dist/ReactToastify.css';
+
+// import background from '../assets/background.svg';
+
+export default createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap');
+
+  *{
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  *:focus{
+    outline: 0;
+  }
+
+  html, body, #root{
+    height: 100%;
+  }
+
+  body{
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button {
+    font-size: 14px;
+    font-family: 'Roboto', sans-serif;
+  }
+
+  a{
+    text-decoration: none;
+  }
+
+  ul{
+    list-style: none;
+  }
+
+  button{
+    cursor: pointer;
+  }
+`;
